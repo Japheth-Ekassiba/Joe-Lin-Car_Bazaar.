bafd2ed1a818f1a6964b960fed2a1b7445f3fb29
Japheth-Ekassiba <japhethochuku5@gmail.com>
2024-06-20 14:51:45 +0200

    Initial commit

---
+++ src/test/java/com/joelin/car_bazaar/CarBazaarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joelin.car_bazaar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarBazaarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
